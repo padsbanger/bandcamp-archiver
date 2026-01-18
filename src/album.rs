e30736c916125b0c88766c562b99793fe48cebf7
Michał Lach <kontakt@michal-lach.pl>
2026-01-18 17:45:34 +0100

    json serialize

--- src/album.rs
+++ src/album.rs
@@ -1,30 +1,35 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Album {
-    title: String,
-    artist: String,
-    songs: Vec<Song>,
+#[serde(rename = "for the curious")]
+    pub for_the_curious: Option<String>,
+    pub current: CurrentRelease,
+    pub artist: String,
+    pub item_type: String,   // "album", "track", etc.
+    pub id: i64,
+    pub album_release_date: String,
+    pub trackinfo: Vec<TrackInfo>,
+    pub url: String,
 }
 
-pub struct Song {
-    file: HasMap<String, String>,
-    title: String,
-    artist: String,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TrackInfo {
+    pub id: i64,
+    pub track_id: i64,
+    pub file: HashMap<String, String>, // "mp3-128" → url, sometimes also "mp3-v0", "flac", etc.
+    pub artist: Option<String>,
+    pub title: String,
+    pub track_num: i32,
+    pub title_link: String,
+    pub duration: f64,
 }
 
-impl Album {
-    fn new() -> Self {
-        Album { songs: Vec::new() }
-    }
-    fn add_song(&mut self, song: Song) {
-        self.songs.push(song);
-    }
-}
 
-impl Song {
-    fn new(file: HashMap, title: String, artist: String) -> Self {
-        Song {
-            file: HashMap::new(),
-            title: String::new(),
-            artist: String::new(),
-        }
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CurrentRelease {
+    pub audit: i32,
+    pub title: String,
+    pub publish_date: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod args;
 mod scrapper;
+mod album;
+
+use album::Album;
 
 fn main() -> anyhow::Result<()> {
     let url = args::Args::parse_args().url().to_string();
@@ -9,7 +12,10 @@ fn main() -> anyhow::Result<()> {
 
     match scraper.fetch_html() {
         Ok(data) => {
-            println!("Fetched data: {}", data);
+            let data: Album = serde_json::from_str(&data)?;
+            println!("Fetched album data successfully:{} - {}", data.current.title, data.artist);
+            println!("Will now attempt to download {} tracks into default directory", data.trackinfo.len());
+            
         }
         Err(e) => {
             return Err(anyhow::anyhow!("Error, no album info found."));
